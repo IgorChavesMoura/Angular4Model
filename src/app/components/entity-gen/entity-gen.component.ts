dfeb7505c0e42b9ff49ee59595d79b66ea14b26e
Igor Moura <igor310597@GMAIL.COM>
2017-08-22 18:11:27 -0300

    radio input box

--- src/app/components/entity-gen/entity-gen.component.ts
+++ src/app/components/entity-gen/entity-gen.component.ts
@@ -10,32 +10,42 @@ import { StyleHelper } from '../../helpers/StyleHelper';
 export class EntityGenComponent implements OnInit {
 
   public many: Array<any> = [
-    { type:'text', name:'conteudo1', title:'Input de texto' },
-    { type:'checkbox', name:'conteudo2', title:'Input de checkbox' },
-    { type:'radio', name:'conteudo3', title:'Input de radio button' },
-    { type:'row', many:[] }
-  
+    { type: 'row', name:'Row' ,many: [] }
+
   ];
   public many2: Array<any> = [];
 
+  public types: Array<any> = [
+    { id: 0, type: 'text' },
+    { id: 1, type: 'password' },  
+    { id: 4, type: 'checkbox' },
+    { id: 5, type: 'radio' },
+    { id: 6, type: 'row' }
+  ];
+
+  public radioValues:Array<number> = [1,2,3,4,5];
+
+  public selectedType = null;
   constructor(private dragula: DragulaService) {
 
   }
 
+
+
   ngOnInit() {
 
-    this.dragula.setOptions('bag-one',{
-      copy:(el,container,handle)=>{
+    this.dragula.setOptions('bag-one', {
+      copy: (el, container, handle) => {
         console.log(container.className);
         let inModels = new RegExp('(?:^|\\s+)' + 'container-models' + '(?:\\s+|$)').test(container.className);
         return inModels;
       },
-      accepts:(el,container,handle)=>{
+      accepts: (el, container, handle) => {
         let acceptable = new RegExp('(?:^|\\s+)' + 'acceptable' + '(?:\\s+|$)').test(el.className);
 
         return acceptable;
       },
-      removeOnSpill:(el,container,handle)=>{
+      removeOnSpill: (el, container, handle) => {
         let removable = new RegExp('(?:^|\\s+)' + 'removable' + '(?:\\s+|$)').test(el.className);
 
         return removable;
@@ -47,7 +57,7 @@ export class EntityGenComponent implements OnInit {
     });
     this.dragula.removeModel.subscribe((value) => {
       this.onRemoveModel(value.slice(1));
-    }); 
+    });
     this.dragula.over.subscribe((value) => {
       this.onOver(value.slice(1));
     });
@@ -55,6 +65,21 @@ export class EntityGenComponent implements OnInit {
       this.onOut(value.slice(1));
     });
 
+  }
+
+  addField(fieldName) {
+    console.log(fieldName);
+    console.log(this.selectedType);
+
+    this.many.push({
+      type:this.selectedType,
+      name:fieldName,
+      title:fieldName
+    });
+
+  }
+  compareTypesbyId(type1, type2) { //Function to select an active option if an user id was passed as param
+    return type1 && type2 ? type1.id === type2.id : type1 === type2;
   }
   private onDropModel(args) {
     let [el, target, source] = args;
